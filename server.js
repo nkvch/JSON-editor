af814ecb08b5a68f13c8e4dd4dff4178bf6a9f72
Stanislau Stankevich <s.stankevich@stud.elka.pw.edu.pl>
2021-06-20 12:55:01 +0200

    qwerty

--- server.js
+++ server.js
@@ -30,8 +30,7 @@ app.use('/jsons', jsons);
 if (process.env.NODE_ENV === 'production') {
     app.use(express.static('client/build'));
     
-    app.get('*', (req, res) => {
-        console.log('running production commands');   
+    app.get('*', (req, res) => {  
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
     });
 }
